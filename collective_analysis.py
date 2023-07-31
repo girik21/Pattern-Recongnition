8277d0f7f5f3010d76143bef73f1bc9d7882974e
girik21 <girikaustuv@gmail.com>
2023-07-31 14:36:08 -0700

    feat: before and after

--- collective_analysis.py
+++ collective_analysis.py
@@ -1,86 +1,43 @@
 import numpy as np
 from binary_image import binary_img
-
-def groupMerge(groupCheck, label):
-    if groupCheck[label] == label:
-        return label
-    groupCheck[label] = groupMerge(groupCheck, groupCheck[label])
-    return groupCheck[label]
-
-def total_group_merger(groupCheck):
-    for i in range(len(groupCheck)):
-        groupCheck[i] = groupMerge(groupCheck, i)
+from scipy import ndimage
 
 def connectivity_analysis(imageTable):
-    rows = len(imageTable)
-    cols = len(imageTable[0])
-    dynamic_image = [[0 for _ in range(cols)] for _ in range(rows)]
-    group_size = rows * cols
-    label_size = 1
-    group_check = [0] * group_size
+    # Convert the array to binary with only background and foreground (Step 4)
+    binary_image = np.where(imageTable > 0, 1, 0)
 
-    # Check the first row and the first column
-    if imageTable[0][0] > 0:
-        dynamic_image[0][0] = label_size
-        group_check[label_size] = label_size
-        label_size += 1
+    # Label the objects in the image (Step 5)
+    labeled_image, num_objects = ndimage.label(binary_image, structure=[[1, 1, 1], [1, 1, 1], [1, 1, 1]])
 
-    for i in range(1, rows):
-        if imageTable[i][0] > 0:
-            if dynamic_image[i-1][0] > 0:
-                dynamic_image[i][0] = dynamic_image[i-1][0]
-            else:
-                dynamic_image[i][0] = label_size
-                group_check[label_size] = label_size
-                label_size += 1
+    # Show labeled image after group merge
+    print('\n')
+    print("After Group Merge:")
+    for row in labeled_image:
+        print(" ".join(map(str, row)))
 
-    for j in range(1, cols):
-        if imageTable[0][j] > 0:
-            if dynamic_image[0][j-1] > 0:
-                dynamic_image[0][j] = dynamic_image[0][j-1]
-            else:
-                dynamic_image[0][j] = label_size
-                group_check[label_size] = label_size
-                label_size += 1
+    # Extract unique labels from the labeled image
+    unique_labels = np.unique(labeled_image)
+    
+    # Create a dictionary to map group numbers to their labels
+    group_labels = {}
+    for group_num, label in enumerate(unique_labels):
+        group_labels[group_num] = label
 
-    for i in range(1, rows):
-        for j in range(1, cols):
-            if imageTable[i][j] > 0:
-                if dynamic_image[i][j-1] > 0:
-                    dynamic_image[i][j] = dynamic_image[i][j-1]
-                elif dynamic_image[i-1][j] > 0:
-                    dynamic_image[i][j] = dynamic_image[i-1][j]
-                else:
-                    dynamic_image[i][j] = label_size
-                    group_check[label_size] = label_size
-                    label_size += 1
-
-    # Merge the same group
-    for i in range(1, label_size):
-        group_check[i] = groupMerge(group_check, i)
-
-    # Merge the groups of the image
-    for i in range(rows):
-        for j in range(cols):
-            if imageTable[i][j] > 0:
-                imageTable[i][j] = group_check[dynamic_image[i][j]]
-
-    total_group_merger(group_check)
-
-    # Show groupCheck
+    # Show the mapping of group numbers to labels
     print("\nGROUP :   LABEL")
-    for i in range(label_size):
-        print(f"{i}     :      {group_check[i]}")
+    for group_num, label in group_labels.items():
+        print(f"{group_num}     :      {label}")
 
-    return imageTable
+    return labeled_image
 
 # Convert binary_img to a NumPy array
 imageTable = np.array(binary_img)
 
+# Show the original image
+print('\n')
+print("Before Group Merge:")
+for row in imageTable:
+    print(" ".join(map(str, row)))
+
 # Apply connectivity analysis
 labeled_image = connectivity_analysis(imageTable)
-
-# Display labeled image in the desired format
-print('\n')
-for row in labeled_image: 
-    print(" ".join(map(str, row)))
